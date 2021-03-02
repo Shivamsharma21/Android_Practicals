f505f3439a2f947a739f396242a7fbf85847fe24
Shivam Sharma <shivam.sharma@berylsystems.com>
2021-03-02 12:06:59 +0530

    Relative Layout Example Is created

---
+++ app/src/main/java/com/example/androidpracticals/RelativeLayoutExample.java
@@ -0,0 +1,16 @@
+package com.example.androidpracticals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeLayoutExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout_example);
+
+
+    }
+}
