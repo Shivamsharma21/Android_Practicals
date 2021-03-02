1a75d823d3dd68605e2f0a4b863ab09f9f22e2f2
Shivam Sharma <shivam.sharma@berylsystems.com>
2021-03-02 10:58:16 +0530

    Android TextView,With LinearLayout Example Is created

--- app/src/main/java/com/example/androidpracticals/LayoutExample.java
+++ app/src/main/java/com/example/androidpracticals/LayoutExample.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LinearLayoutExample extends AppCompatActivity {
+public class LayoutExample extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear_layout_example);
+        setContentView(R.layout.activity_layout_example);
     }
 }
